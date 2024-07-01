b28d0d0d19c03fe3cd13ac493b66cefa6a68ebb6
angel melendez <angelmelen88@gmail.com>
2024-07-01 15:56:27 -0600

    feat:notacion useclient and add component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+// "use client"
+
 import Image from "next/image";
 
 export default function Home() {
+  console.log('use client')
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-3xl">hola mundo</h1>
